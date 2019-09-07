3d07fc542a91b292fbac5ff62f8dd9ef7b5069ee
jlefevre <jlefevre@soe.ucsc.edu>
2019-09-07 14:41:16 -0700

    Load data scripts for all 10MB raw obj sizes.

--- scripts/load-data.sh
+++ scripts/load-data.sh
@@ -5,18 +5,27 @@ nosds=$3
 nobjs=$4
 
 
+echo "pool=${pool}";
+echo "objfile=${objfile}";
+echo "nosds=${nosds}";
+echo "nobjs=${nobjs}";
+
+
+
+
 cd $HOME
 rados rmpool $pool $pool --yes-i-really-really-mean-it
 rados mkpool $pool
 
 # magic number, seems work well at all data sizes 10MB/100MB and cluster scales 1,2,4
 groupsize_multiplier=20
-
 groupsize=$(($groupsize_multiplier*$nosds))
-echo $groupsize
+echo "groupsize=${groupsize}";
 groupsize=$(( groupsize < nobjs ? groupsize : nobjs ))
-for ((i=0; i < $nobjs; i+=groupsize)); do
-    for ((j=0; j < groupsize; j++)); do
+echo "groupsize=${groupsize}";
+echo "nobjs=${nobjs}";
+for ((i=0; i<nobjs; i+=groupsize)); do
+    for ((j=0; j<groupsize; j++)); do
         oid=$(($i+$j))
         if [ "${oid}" -ge "${nobjs}" ]
         then
